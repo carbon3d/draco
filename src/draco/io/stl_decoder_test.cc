068695d5d4e14460bcf44c6a79fe87f68f8556f3
Nicholas Harrington <nharrington@carbon3d.com>
2019-02-19 16:32:10 -0800

    Added more test cases for stl decoding

--- src/draco/io/stl_decoder_test.cc
+++ src/draco/io/stl_decoder_test.cc
@@ -31,21 +31,43 @@ class StlDecoderTest : public ::testing::Test {
       return nullptr;
     return geometry;
   }
-  void test_decoding(const std::string &file_name) {
+  void test_decoding(const std::string &file_name, int expected_num_faces) {
     const std::unique_ptr<Mesh> mesh(DecodeStl<Mesh>(file_name));
     ASSERT_NE(mesh, nullptr) << "Failed to load test model " << file_name;
-    ASSERT_GT(mesh->num_faces(), 0);
+    ASSERT_EQ(mesh->num_faces(), expected_num_faces);
+  }
+  void check_failure_to_decode(const std::string &file_name) {
+    const std::unique_ptr<Mesh> mesh(DecodeStl<Mesh>(file_name));
+    ASSERT_FALSE(mesh);
   }
 };
 
 TEST_F(StlDecoderTest, ValidBinarySTL) {
-  const std::string file_name = "teapot.stl";
-  test_decoding(file_name);
+  test_decoding("teapot.stl", 946);
+  test_decoding("40mmcube.stl", 946);
 }
 
 TEST_F(StlDecoderTest, ValidAsciiSTL) {
-  const std::string file_name = "sphere.stl";
-  test_decoding(file_name);
+  test_decoding("ascii_sphere.stl", 228);
+  test_decoding("ascii_block100.stl", 12);
+  test_decoding("ascii_facet_name.stl", 12);
+  test_decoding("ascii_noname.stl", 12);
+}
+
+TEST_F(StlDecoderTest, InfinityBinarySTL) {
+  const std::string file_name = "invalid/inf_teapot.stl";
+  test_decoding(file_name, 946);
+}
+
+TEST_F(StlDecoderTest, NaNBinarySTL) {
+  const std::string file_name = "invalid/nan_teapot.stl";
+  test_decoding(file_name, 946);
+}
+
+TEST_F(StlDecoderTest, ImproperNumberBinarySTL) {
+  const std::string file_name = "invalid/tri_num_too_small_teapot.stl";
+  test_decoding(file_name, 926);
+  check_failure_to_decode("invalid/tri_num_too_large_teapot.stl");
 }
 
 }  // namespace draco
