3bfae9ca5de4580db3be1316293cca7d2f2f3413
Nicholas Harrington <nharrington@carbon3d.com>
2019-03-20 10:43:36 -0700

    Finished a first pass at writing the new embind based bindings

--- src/draco/javascript/emscripten/draco_embind_encoder.cc
+++ src/draco/javascript/emscripten/draco_embind_encoder.cc
@@ -16,13 +16,15 @@
 #include <emscripten/val.h>
 
 #include <math.h>
-
 #include <iostream>
 
+#include "draco/compression/encode.h"
 #include "draco/core/decoder_buffer.h"
 #include "draco/io/mesh_io.h"
 #include "draco/mesh/mesh.h"
+#include "draco/point_cloud/point_cloud.h"
 
+typedef draco::GeometryAttribute::Type draco_GeometryAttribute_Type;
 
 class MeshQuantizationCarbon {
  public:
@@ -94,7 +96,6 @@ class DecoderBufferOwner {
   emscripten::val GetBufferView() {
     return emscripten::val(emscripten::typed_memory_view(buffer_.size(), &(buffer_[0])));
   }
-  
  private:
   draco::DecoderBuffer decoder_buffer_;
   std::vector<char> buffer_;
@@ -114,22 +115,158 @@ bool DecodeFileBufferToMesh(draco::DecoderBuffer* buffer,
   return status.ok();
 }
 
+class DracoInt8Array {
+ public:
+  DracoInt8Array() {}
+  int8_t GetValue(int index) const {return values_[index];}
+  bool SetValues(const char *values, int count) {
+    values_.assign(values, values + count);
+    return true;
+  }
+  size_t size() { return values_.size(); }
+  emscripten::val GetView() {
+    return emscripten::val(emscripten::typed_memory_view(values_.size(), &(values_[0])));
+  }
+  
+ private:
+  std::vector<int8_t> values_;
+};
+
+
+class Encoder {
+ public:
+  Encoder();
+
+  void SetEncodingMethod(long method);
+  void SetAttributeQuantization(draco_GeometryAttribute_Type type,
+                                long quantization_bits);
+  void SetAttributeExplicitQuantization(draco_GeometryAttribute_Type type,
+                                        long quantization_bits,
+                                        long num_components,
+                                        const float *origin, float range);
+  void SetSpeedOptions(long encoding_speed, long decoding_speed);
+  void SetTrackEncodedProperties(bool flag);
+
+  int EncodeMeshToDracoBuffer(draco::Mesh *mesh, DracoInt8Array *buffer);
+
+  int EncodePointCloudToDracoBuffer(draco::PointCloud *pc,
+                                    bool deduplicate_values,
+                                    DracoInt8Array *buffer);
+  int GetNumberOfEncodedPoints();
+  int GetNumberOfEncodedFaces();
+
+ private:
+  draco::Encoder encoder_;
+};
+
+Encoder::Encoder() {}
+
+void Encoder::SetEncodingMethod(long method) {
+  encoder_.SetEncodingMethod(method);
+}
+
+void Encoder::SetAttributeQuantization(draco_GeometryAttribute_Type type,
+                                       long quantization_bits) {
+  encoder_.SetAttributeQuantization(type, quantization_bits);
+}
+
+void Encoder::SetAttributeExplicitQuantization(
+    draco_GeometryAttribute_Type type, long quantization_bits,
+    long num_components, const float *origin, float range) {
+  encoder_.SetAttributeExplicitQuantization(type, quantization_bits,
+                                            num_components, origin, range);
+}
+
+void Encoder::SetSpeedOptions(long encoding_speed, long decoding_speed) {
+  encoder_.SetSpeedOptions(encoding_speed, decoding_speed);
+}
+
+void Encoder::SetTrackEncodedProperties(bool flag) {
+  encoder_.SetTrackEncodedProperties(flag);
+}
+
+int Encoder::EncodeMeshToDracoBuffer(draco::Mesh *mesh, DracoInt8Array *draco_buffer) {
+  if (!mesh)
+    return 0;
+  draco::EncoderBuffer buffer;
+  if (mesh->GetNamedAttributeId(draco::GeometryAttribute::POSITION) == -1)
+    return 0;
+  if (!mesh->DeduplicateAttributeValues())
+    return 0;
+  mesh->DeduplicatePointIds();
+  if (!encoder_.EncodeMeshToBuffer(*mesh, &buffer).ok()) {
+    return 0;
+  }
+  draco_buffer->SetValues(buffer.data(), buffer.size());
+  return buffer.size();
+}
+
+int Encoder::EncodePointCloudToDracoBuffer(draco::PointCloud *pc,
+                                           bool deduplicate_values,
+                                           DracoInt8Array *draco_buffer) {
+  // TODO(ostava): Refactor common functionality with EncodeMeshToDracoBuffer().
+  if (!pc)
+    return 0;
+  draco::EncoderBuffer buffer;
+  if (pc->GetNamedAttributeId(draco::GeometryAttribute::POSITION) == -1)
+    return 0;
+  if (deduplicate_values) {
+    if (!pc->DeduplicateAttributeValues())
+      return 0;
+    pc->DeduplicatePointIds();
+  }
+  if (!encoder_.EncodePointCloudToBuffer(*pc, &buffer).ok()) {
+    return 0;
+  }
+  draco_buffer->SetValues(buffer.data(), buffer.size());
+  return buffer.size();
+}
+
+int Encoder::GetNumberOfEncodedPoints() {
+  return encoder_.num_encoded_points();
+}
+
+int Encoder::GetNumberOfEncodedFaces() { return encoder_.num_encoded_faces(); }
+
 EMSCRIPTEN_BINDINGS(dracoEncoder) {
-  emscripten::class_<draco::Mesh>("Mesh")
+  emscripten::class_<DracoInt8Array>("DracoInt8Array")
+      .constructor<>()
+      .function("GetView", &DracoInt8Array::GetView)
+      ;
+
+  emscripten::enum_<draco_GeometryAttribute_Type>("draco_GeometryAttribute_Type")
+      .value("INVALID", draco::GeometryAttribute::INVALID)
+      .value("POSITION", draco::GeometryAttribute::POSITION)
+      .value("NORMAL", draco::GeometryAttribute::NORMAL)
+      .value("COLOR", draco::GeometryAttribute::COLOR)
+      .value("TEX_COORD", draco::GeometryAttribute::TEX_COORD)
+      .value("GENERIC", draco::GeometryAttribute::GENERIC)
+      ;
+
+  emscripten::class_<draco::PointCloud>("PointCloud")
+      .constructor<>()
+      .function("num_attributes", &draco::PointCloud::num_attributes)
+      .function("num_points", &draco::PointCloud::num_points)
+      ;
+
+  emscripten::class_<draco::Mesh, emscripten::base<draco::PointCloud>>("Mesh")
       .constructor<>()
       .function("num_faces", &draco::Mesh::num_faces)
       ;
+
   emscripten::class_<draco::DecoderBuffer>("DecoderBuffer")
       .constructor<>()
       .function("Init", emscripten::select_overload<void(const char*, size_t)>(
           &draco::DecoderBuffer::Init), emscripten::allow_raw_pointers());
       ;
+
   emscripten::class_<DecoderBufferOwner>("DecoderBufferOwner")
       .constructor<size_t>()
       .function("GetDecoderBuffer", &DecoderBufferOwner::GetDecoderBuffer,
                 emscripten::allow_raw_pointers())
       .function("GetBufferView", &DecoderBufferOwner::GetBufferView)
       ;
+
   emscripten::class_<MeshQuantizationCarbon>("MeshQuantizationCarbon")
       .constructor<>()
       .function("IsSet", &MeshQuantizationCarbon::IsSet)
@@ -141,6 +278,23 @@ EMSCRIPTEN_BINDINGS(dracoEncoder) {
       .function("FillFromMesh", &MeshQuantizationCarbon::FillFromMesh,
                 emscripten::allow_raw_pointers())
       ;
+
   emscripten::function("DecodeFileBufferToMesh", DecodeFileBufferToMesh,
                        emscripten::allow_raw_pointers());
+
+  emscripten::class_<Encoder>("Encoder")
+      .constructor<>()
+      .function("SetEncodingMethod", &Encoder::SetEncodingMethod)
+      .function("SetAttributeQuantization", &Encoder::SetAttributeQuantization)
+      .function("SetAttributeExplicitQuantization", &Encoder::SetAttributeExplicitQuantization,
+                emscripten::allow_raw_pointers())
+      .function("SetSpeedOptions", &Encoder::SetSpeedOptions)
+      .function("SetTrackEncodedProperties", &Encoder::SetTrackEncodedProperties)
+      .function("EncodeMeshToDracoBuffer", &Encoder::EncodeMeshToDracoBuffer,
+                emscripten::allow_raw_pointers())
+      .function("EncodePointCloudToDracoBuffer", &Encoder::EncodePointCloudToDracoBuffer,
+                emscripten::allow_raw_pointers())
+      .function("GetNumberOfEncodedPoints", &Encoder::GetNumberOfEncodedPoints)
+      .function("GetNumberOfEncodedFaces", &Encoder::GetNumberOfEncodedFaces)
+      ;
 }
